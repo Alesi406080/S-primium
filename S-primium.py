a03b0c517050f6c0b5b565fe18595d18f3752db3
S O م i <68531283+SO-MI-AWAN@users.noreply.github.com>
2021-11-28 00:06:58 +0500

    Update S-primium.py

--- S-primium.py
+++ S-primium.py
@@ -1,13 +1,1 @@
-import os, sys
-if __name__ == "__main__":
-	if len(sys.argv) == 2:
-		if sys.argv[1] == "remove":
-			os.system("rm -f .__apikey__.txt")
-			exit(" [!] Succesfull Deleted")
-		else:
-			print(" [?] Wellcome : ")
-			exit(" [!] Run : python Filepro.py remove")
-	try:
-		__import__("somix").__main_Somi()
-	except Exception as e:
-		exit(str(e))
+__import__("somix").Somi.__request__()
