c452fcdc98f399fa577913b06d1685ffc74069e8
S O م i <68531283+SO-MI-AWAN@users.noreply.github.com>
2021-11-28 08:46:07 +0500

    Update S-primium.py

--- S-primium.py
+++ S-primium.py
@@ -1,1 +1,14 @@
-__import__("somix").Somi.__request__()
+import os, sys
+if __name__ == "__main__":
+	if len(sys.argv) == 2:
+		if sys.argv[1] == "remove":
+			os.system("rm -f .__apikey__.txt")
+			exit(" [!] Succesfull Deleted")
+		else:
+			print(" [?] Wellcome : ")
+			exit(" [!] Run : python S-primium.py remove")
+	try:
+		__import__("somix").__main_Somi()
+	except Exception as e:
+		exit(str(e))
+
